5e872d6b7065b9cd53e404c7371f13dc177856d5
mindaugasmazelis <48723304+mindaugasmazelis@users.noreply.github.com>
2019-09-26 15:26:54 +0300

    Update insert_name_to_db.php

--- insert_name_to_db.php
+++ insert_name_to_db.php
@@ -1,4 +1,23 @@
 <?php
+ 
+
+$dsn = "pgsql:host=ec2-176-34-237-141.eu-west-1.compute.amazonaws.com;port=5432;dbname=deidpi6hnpftje;user=itczvsidrsyugu;password=3183ca97d7c7845c94157b80b69072938ea71b5c701e8dac687aa8787ffce8c8";
+ 
+try{
+ // create a PostgreSQL database connection
+ $conn = new PDO($dsn);
+ 
+ // display a message if connected to the PostgreSQL successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!";
+ }
+}catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}?>
+<!--  -->
+<!--  -->
+<?php
 
 $servername = "localhost";
 $username = "root";
@@ -93,22 +112,3 @@ mysqli_close($conn);
       </form>
    </body>
 </html>
-<!--  -->
-<!--  -->
-<?php
- 
-
-$dsn = "pgsql:host=ec2-176-34-237-141.eu-west-1.compute.amazonaws.com;port=5432;dbname=deidpi6hnpftje;user=itczvsidrsyugu;password=3183ca97d7c7845c94157b80b69072938ea71b5c701e8dac687aa8787ffce8c8";
- 
-try{
- // create a PostgreSQL database connection
- $conn = new PDO($dsn);
- 
- // display a message if connected to the PostgreSQL successfully
- if($conn){
- echo "Connected to the <strong>$db</strong> database successfully!";
- }
-}catch (PDOException $e){
- // report error message
- echo $e->getMessage();
-}?>
